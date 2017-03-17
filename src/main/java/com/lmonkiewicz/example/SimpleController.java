541821c6b066507686b8f022c87b011c50918206
Łukasz Monkiewicz <lmonkiewicz@gmail.com>
2017-03-18 00:18:14 +0100

    Moved users endpoint to separate controller

--- src/main/java/com/lmonkiewicz/example/SimpleController.java
+++ src/main/java/com/lmonkiewicz/example/SimpleController.java
@@ -1,12 +1,7 @@
 package com.lmonkiewicz.example;
 
-import com.lmonkiewicz.example.model.Person;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.net.URI;
 
 /**
  * Created by lmonkiewicz on 17.03.2017.
@@ -33,10 +28,5 @@ public class SimpleController {
         else {
             return ResponseEntity.notFound().build();
         }
-    }
-
-    @PostMapping("/users")
-    private ResponseEntity<?> createPerson(@Valid @RequestBody Person person) {
-        return ResponseEntity.created(URI.create("/simple/users/1")).body("1");
     }
 }
